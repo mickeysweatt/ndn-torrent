f7651c2eed20aaee49090d1cb3aed97a14c20bb4
Mickey Sweatt <sweattm92@gmail.com>
2015-02-14 20:31:59 -0800

    Implemented multi-file torent parsing

--- src/torrentParser.cpp
+++ src/torrentParser.cpp
@@ -43,20 +43,50 @@ namespace torrent {
        return infoDict.end() == infoDict.find("length");
     }
     
-    static void getInfo(const BencodeDict& infoDict,
-                        int&               fileLength,
-                        std::string&       name,
-                        int&               pieceLength,
-                        std::vector<char>& pieces)
+    static void getInfoCommon(const BencodeDict& infoDict,
+                              std::string&       name,
+                              int&               pieceLength,
+                              std::vector<char>& pieces)
     {
-        auto fileLengthToken  = dynamic_cast<const IntegerToken    *>(infoDict.find("length")->second);
         auto pieceLengthToken = dynamic_cast<const IntegerToken    *>(infoDict.find("piece length")->second);
         auto nameToken        = dynamic_cast<const ByteStringToken *>(infoDict.find("name")->second);
         auto piecesToken      = dynamic_cast<const ByteStringToken *>(infoDict.find("pieces")->second);
         name        = nameToken->getString();
         pieceLength = pieceLengthToken->getValue();
-        fileLength  = fileLengthToken->getValue();
         pieces      = piecesToken->getValue();
+    }
+    
+    static void getInfoMultiFileTorrent(const BencodeDict& infoDict,
+                                        std::set<std::pair<std::string, size_t>> files,
+                                        std::string&       name,
+                                        int&               pieceLength,
+                                        std::vector<char>& pieces)
+    {
+        auto fileListToken  = dynamic_cast<const BencodeList *>(infoDict.find("files")->second);
+        for (auto file : fileListToken->getTokens()) {
+            auto fileDict = dynamic_cast<const BencodeDict&>(*file);
+            std::string pathString = "";
+            auto path = dynamic_cast<const BencodeList &>(fileDict["path"]);
+            auto pathCompnents = path.getTokens();
+            for (auto pathComponent : path.getTokens()) {
+                pathString += dynamic_cast<const ByteStringToken *>(pathComponent)->getString() +
+                (pathComponent == pathCompnents.back() ? "" : "/" );
+            }
+            auto length = dynamic_cast<const IntegerToken &>(fileDict["length"]).getValue();
+            files.insert(std::pair<string, size_t>(pathString, length));
+        }
+        getInfoCommon(infoDict, name, pieceLength, pieces);
+    }
+    
+    static void getInfoSingleFileTorrent(const BencodeDict& infoDict,
+                                         int&               fileLength,
+                                         std::string&       name,
+                                         int&               pieceLength,
+                                         std::vector<char>& pieces)
+    {
+        getInfoCommon(infoDict, name, pieceLength, pieces);
+        auto fileLengthToken  = dynamic_cast<const IntegerToken *>(infoDict.find("length")->second);
+        pieceLength = fileLengthToken->getValue();
     }
 
     Torrent&& TorrentParserUtil::parseFile(std::istream& in)
@@ -84,9 +114,9 @@ namespace torrent {
                                                                    torrentDict["announce-list"]));
         announceList.insert(announceToken->getString());
         if (isMultieFileTorrent(dynamic_cast<BencodeDict&>(*torrentDict["info"]))) {
-            throw new TorrentParserUtil::ParseError("Implement me");
+            getInfoMultiFileTorrent(dynamic_cast<BencodeDict&>(*torrentDict["info"]), files, name, piece_length, pieces);
         }
-        getInfo(dynamic_cast<BencodeDict&>(*torrentDict["info"]), fileLength, name, piece_length, pieces);
+        getInfoSingleFileTorrent(dynamic_cast<BencodeDict&>(*torrentDict["info"]), fileLength, name, piece_length, pieces);
         return std::move(t);
      }
 }
--- src/torrentParser.t.cpp
+++ src/torrentParser.t.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 
  int main() {
-     std::ifstream ifs("/Users/admin/Desktop/ubuntu-14.10-desktop-amd64.torrent", std::ios::binary);
-     torrent::TorrentParserUtil::parseFile(ifs);
+     std::ifstream singleFileTorrent("/Users/admin/Desktop/ubuntu-14.10-desktop-amd64.torrent", std::ios::binary);
+     std::ifstream manyFileTorrent("/Users/admin/Desktop/groks113_archive.torrent", std::ios::binary);
+     torrent::TorrentParserUtil::parseFile(singleFileTorrent);
+     torrent::TorrentParserUtil::parseFile(manyFileTorrent);
+     
  }
