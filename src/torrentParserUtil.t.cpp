1ddc6614dac8942444a8513704b237a7abaca4bf
Mickey Sweatt <sweattm92@gmail.com>
2015-02-24 19:46:37 -0800

    Added test files to common dir

--- src/torrentParserUtil.t.cpp
+++ src/torrentParserUtil.t.cpp
@@ -5,14 +5,18 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
+#include <iostream>
 
-#define PREFIX "/Users/admin/Desktop/"
+#define PREFIX "/Users/admin/Dropbox/ndn-torrent/test/"
+
+#include <unistd.h>
 
  int main() {
      std::ifstream single(PREFIX "ubuntu-14.10-desktop-amd64.torrent",
                           std::ios::binary);
      std::ifstream many(PREFIX "groks113_archive.torrent",
-                           std::ios::binary);
+                        std::ios::binary);
+
      torrent::TorrentParserUtil::parseFile(single);
      torrent::TorrentParserUtil::parseFile(many);
  }
